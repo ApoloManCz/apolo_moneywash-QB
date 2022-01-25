34c5104a7c0db117154a76e0566f4a4d6cb941ac
Apolo <75501135+ApoloManCz@users.noreply.github.com>
2022-01-25 16:17:55 +0100

    Add files via upload

---
+++ apolo_moneywash/fxmanifest.lua
@@ -0,0 +1,11 @@
+--Made by Apolo ♡#1111 !
+--Dont share without my permissions !
+fx_version 'cerulean'
+game 'gta5'
+
+description 'QB-MoneyWash by Apolo'
+version '1.0.0'
+
+shared_script '@qb-core/shared.lua'
+server_script 'server/main.lua'
+client_script 'client/main.lua'
