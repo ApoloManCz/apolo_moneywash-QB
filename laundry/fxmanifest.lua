5fa1f4267c3f80c19d8d1bfa3071d7a0ca41635d
Apolo <75501135+ApoloManCz@users.noreply.github.com>
2022-01-25 16:20:40 +0100

    Create fxmanifest.lua

---
+++ laundry/fxmanifest.lua
@@ -0,0 +1,23 @@
+fx_version 'bodacious'
+game 'gta5'
+
+author 'DPS'
+description 'Add-on Laundromat Interior'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+
+data_file 'TIMECYCLEMOD_FILE' 'laun_timecycle_mods_1.xml'
+data_file 'AUDIO_GAMEDATA' 'audio/dps_v_laund_game.dat'
+data_file 'AUDIO_DYNAMIXDATA' 'audio/dps_v_laund_mix.dat'
+data_file 'INTERIOR_PROXY_ORDER_FILE' 'interiorproxies.meta'
+
+files {
+	'laun_timecycle_mods_1.xml',
+	'audio/dps_v_laund_game.dat151.rel',
+	'audio/dps_v_laund_mix.dat15.rel',
+	'interiorproxies.meta'
+}
+
+
