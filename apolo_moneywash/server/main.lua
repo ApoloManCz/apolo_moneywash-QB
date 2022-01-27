317b25e70222e35d52bbc5a26bc3d28d27f96777
Apolo <75501135+ApoloManCz@users.noreply.github.com>
2022-01-27 19:46:04 +0100

    Update main.lua

--- apolo_moneywash/server/main.lua
+++ apolo_moneywash/server/main.lua
@@ -7,39 +7,32 @@ local ItemList = {
 RegisterServerEvent('apolo_moneywash:server:getmoney')
 AddEventHandler('apolo_moneywash:server:getmoney', function()
     local src = source
-    local Player = QBCore.Functions.GetPlayer(src)
-    local markedbills = Player.Functions.GetItemByName('markedbills')
 	local total_worth = 0
-	
-    if Player.PlayerData.items ~= nil then 
-        for k, v in pairs(Player.PlayerData.items) do 
-            if markedbills ~= nil then
-				for itemkey, item in pairs(Player.PlayerData.items) do
-                if ItemList[Player.PlayerData.items[k].name] ~= nil then 
-					if type(item.info) ~= 'string' and tonumber(item.info.worth) then
-					total_worth = total_worth + tonumber(item.info.worth)
-                    if Player.PlayerData.items[k].name == "markedbills" and Player.PlayerData.items[k].amount >= 1 then 
-                        local amount = Player.PlayerData.items[k].amount
-						TriggerClientEvent('QBCore:Notify', src, "You washed "..total_worth * amount.." Marked Money!")
-                        Player.Functions.RemoveItem("markedbills", Player.PlayerData.items[k].amount)
-                        TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['markedbills'], "remove")
-                        Player.Functions.AddMoney("cash", total_worth * amount)
-                        break
+	local Player = QBCore.Functions.GetPlayer(tonumber(source))
+    local markedbills = Player.Functions.GetItemByName('markedbills')
+	for itemkey, item in pairs(Player.PlayerData.items) do
+		if type(item.info) ~= 'string' and tonumber(item.info.worth) then
+			total_worth = total_worth + tonumber(item.info.worth)
+	if Player.PlayerData.items ~= nil then
+        if markedbills ~= nil then
+            if markedbills.amount >= 1 then
+                Player.Functions.RemoveItem("markedbills", 1, false)
+                TriggerClientEvent('inventory:client:ItemBox', source, QBCore.Shared.Items['markedbills'], "remove")
+         
+                Player.Functions.AddMoney("cash", total_worth)
+				TriggerClientEvent('QBCore:Notify', src, "You washed "..total_worth.." Marked Money!")             
+				break
                     else
-                        TriggerClientEvent('QBCore:Notify', src, "You do not have marked money", 'error')
-                        break
-                    end
-                end
+                        TriggerClientEvent('QBCore:Notify', src, "You do not have marked money", 'error')     
+                    end              
             else
-                TriggerClientEvent('QBCore:Notify', src, "You do not have marked money", 'error')
-                break
-
+                TriggerClientEvent('QBCore:Notify', src, "You do not have marked money", 'error')               
             end
         end
-    end
-end
+	end
 end
 end)
+
 
 RegisterServerEvent('apolo_moneywash:server:checkmoney')
 AddEventHandler('apolo_moneywash:server:checkmoney', function()
