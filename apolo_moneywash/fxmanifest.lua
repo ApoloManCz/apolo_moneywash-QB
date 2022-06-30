7aecac36d368e90f8f9589d7b55c0aed4c1f8395
Apolo <75501135+ApoloManCz@users.noreply.github.com>
2022-06-30 15:35:48 +0200

    Update fxmanifest.lua

--- apolo_moneywash/fxmanifest.lua
+++ apolo_moneywash/fxmanifest.lua
@@ -1,11 +1,12 @@
---Made by Apolo ♡#1111 !
+--Made by Apolo ♡#9745 !
 --Dont share without my permissions !
-fx_version 'cerulean'
+fx_version 'adamant'
 game 'gta5'
 
+author 'Apolo#9745'
 description 'QB-MoneyWash by Apolo'
 version '1.0.0'
 
-shared_script '@qb-core/shared.lua'
+shared_script '@qb-core/shared/items.lua'
 server_script 'server/main.lua'
 client_script 'client/main.lua'
